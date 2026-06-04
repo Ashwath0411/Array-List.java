918844633d9a22a0cd0176b42102e35798808e2e
Ashwath <ashwath0411@gmail.com>
2026-06-04 14:26:31 +0530

    Create linkedList.java

---
+++ linkedList.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+class link{
+    static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+        list.add("java");
+        list.add("Python");
+        list.add("C++");
+        System.out.println(list);
+    }
+}
