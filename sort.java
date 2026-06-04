d7bc37a59d2833ce107fbac52defae6b794a6870
Ashwath <ashwath0411@gmail.com>
2026-06-04 10:42:08 +0530

    Create sort.java

---
+++ sort.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class sort {
+    static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(40);
+        list.add(10);
+        list.add(30);
+        Collections.sort(list);
+        System.out.println(list);
+    }
+}
