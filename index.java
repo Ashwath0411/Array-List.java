940e1f8827802c30da6543a563823cd7b5fe9a3f
Ashwath <ashwath0411@gmail.com>
2026-06-04 10:31:21 +0530

    Create index.java

---
+++ index.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+class index {
+    static void main(String[] args) {
+        ArrayList<String> fruits = new ArrayList<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Mango");
+        System.out.println(fruits.indexOf("Banana"));
+    }
+}
